73a87030c1e2cb494111867921118aff30c249ec
diegonicita <diegonicita@gmail.com>
2022-06-28 14:18:09 -0300

    agregando logica al checkbox

--- src/Pages/Stepper1/Steps/Step4.jsx
+++ src/Pages/Stepper1/Steps/Step4.jsx
@@ -6,10 +6,9 @@ import { Link } from "react-router-dom";
 import CheckBoxField from "../../../components/CheckBoxField";
 import RadioButtonField from "../../../components/RadioButtonField";
 
-function Step4({ data, updateStep }) {  
-
+function Step4({ data, updateStep }) {
   const onSubmit = () => {
-    console.log("submit 4");    
+    console.log("submit 4");
     sessionStorage.setItem("step4", JSON.stringify({ ...values }));
     updateStep(5);
   };
@@ -19,8 +18,8 @@ function Step4({ data, updateStep }) {
     condicionesSOIPEPFATCA2: data?.condicionesSOIPEPFATCA2 || false,
     condicionesSOIPEPFATCA3: data?.condicionesSOIPEPFATCA3 || false,
     condicionesSOIPEPFATCA4: data?.condicionesSOIPEPFATCA4 || false,
-    tributaEnOtroPais: data?.tributaEnOtroPais || false,    
-    fondosOrigenesLicitos: data?.fondosOrigenesLicitos || false,    
+    tributaEnOtroPais: data?.tributaEnOtroPais || false,
+    fondosOrigenesLicitos: data?.fondosOrigenesLicitos || false,
   };
 
   const formik = useFormik({
@@ -29,28 +28,60 @@ function Step4({ data, updateStep }) {
     onSubmit,
     enableReinitialize: true,
   });
-  const { handleSubmit, handleChange, handleBlur, errors, touched, values } =
-    formik;
+
+  const handleChangeCheckBoxs = (e) => {
+    setFieldValue(e.target.name, e.target.checked);
+    if (
+      e.target.name === "condicionesSOIPEPFATCA4" &&
+      e.target.checked === true
+    ) {
+      setFieldValue("condicionesSOIPEPFATCA1", false);
+      setFieldValue("condicionesSOIPEPFATCA2", false);
+      setFieldValue("condicionesSOIPEPFATCA3", false);
+    }
+
+    if (
+      e.target.name !== "condicionesSOIPEPFATCA4" &&
+      e.target.checked === true
+    ) {
+      setFieldValue("condicionesSOIPEPFATCA4", false);      
+    }
+  };
+
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    errors,
+    touched,
+    values,
+    setFieldValue,
+  } = formik;
 
   return (
     <form onSubmit={handleSubmit} type="POST">
       <CheckBoxField
         label="¿Cumplís alguna de las siguientes condiciones?"
         name="condicionesSOIPEPFATCA"
-        handleChange={handleChange}
+        handleChange={handleChangeCheckBoxs}
         handleBlur={handleBlur}
         errors={errors}
         touched={touched}
         values={values}
         placeholder="None"
         type="checkbox"
-        options={["SOI", "PEP", "FATCA", "Ninguna"]}        
+        options={["SOI", "PEP", "FATCA", "Ninguna"]}
       ></CheckBoxField>
-      <div style={{fontSize: "0.8rem", color: "gray", fontWeight: "800"}}>
-        ¿No estás seguro? 
-        <Link style={{fontSize: "0.8rem", color: "#1488E9", fontWeight: "800"}} to="/"> Ver más informacion
+      <div style={{ fontSize: "0.8rem", color: "gray", fontWeight: "800" }}>
+        ¿No estás seguro?
+        <Link
+          style={{ fontSize: "0.8rem", color: "#1488E9", fontWeight: "800" }}
+          to="/"
+        >
+          {" "}
+          Ver más informacion
         </Link>
-       </div>
+      </div>
       <RadioButtonField
         label="¿Tributás en otro país además de Argentina?"
         name="tributaEnOtroPais"
@@ -58,7 +89,7 @@ function Step4({ data, updateStep }) {
         handleBlur={handleBlur}
         errors={errors}
         touched={touched}
-        values={values.tributaEnOtroPais}        
+        values={values.tributaEnOtroPais}
         options={["Si", "No"]}
       ></RadioButtonField>
       <RadioButtonField
@@ -68,11 +99,14 @@ function Step4({ data, updateStep }) {
         handleBlur={handleBlur}
         errors={errors}
         touched={touched}
-        values={values.fondosOrigenesLicitos}        
+        values={values.fondosOrigenesLicitos}
         options={["Si", "No"]}
-      ></RadioButtonField>     
+      ></RadioButtonField>
 
-      <button type="submit"><span className="icon" style={{left: "15%",top:"5%"}}></span>Proximo Paso</button>
+      <button type="submit">
+        <span className="icon" style={{ left: "15%", top: "5%" }}></span>Proximo
+        Paso
+      </button>
       <Link to="/stepper1" onClick={() => updateStep(3)}>
         Volver
       </Link>
